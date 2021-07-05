53ab3a107da9a97abe726099fad1660f6648bb9c
kristian diez <isnomatophobic@gmail.com>
2021-07-06 01:27:24 +0300

    changed port

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true, useUnifiedTo
 })
 
 
-app.listen(4000,()=>{
+app.listen(process.env.PORT || 4000,()=>{
     console.log(`listening to port 4000...`);
 })

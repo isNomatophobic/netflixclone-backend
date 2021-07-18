d8a6636b9428780f32f5e0aa24f915fb695a1475
kristian diez <isnomatophobic@gmail.com>
2021-07-18 14:13:37 +0300

    make it better

--- routes/login.js
+++ routes/login.js
@@ -19,13 +19,13 @@ router.post('/',async (req,res)=>{
         
         // res.status(200).json(user)
 })
-//Done
+//Get Accounts
 router.get('/accounts',async (req,res)=>{
         const accounts = await Account.find({email:req.query.email})
         console.log(accounts)
         res.json(accounts)
 })
-//Create Account --> *
+//Create Account 
 router.post('/accounts',async (req,res)=>{
         const accounts = await Account.find({email:req.body.email})
         const matching = accounts.filter((account)=>{
@@ -59,6 +59,26 @@ router.post('/accounts',async (req,res)=>{
 
 
 })
+//Delete Account 
+router.delete('/accounts',async (req,res)=>{
+        const account = await Account.findOne({email:req.body.email,accountName:req.body.accountName})
+        console.log(account)
+        try{
+        const token = req.body.jwt
+        const verified = jwt.verify(token,process.env.TOKEN_SECRET)
+       }
+       catch(e){
+               return res.status(400).json({error:"Invalid JWT"})
+        }
+        if(account)
+        {
+         const deletedAccount = await Account.remove(account);
+         res.status(200).json(deletedAccount)
+        }
+        else
+        res.status(404).json({Error:"not found"})
 
+        
+})
 
 export default router
